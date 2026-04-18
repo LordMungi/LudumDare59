f86610e47059f64460eaba0140d7e53862842d72
LordMungi <lrdmungi@gmail.com>
2026-04-18 15:09:27 -0300

    Create handbrake and steering wheel

---
+++ Assets/Scripts/Handbrake.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Handbrake : MonoBehaviour
+{
+    [field: SerializeField] public bool isPressed { get; private set; } = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
