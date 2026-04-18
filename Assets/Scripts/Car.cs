d04f799d0fe31295d3c0930b5cf4d6f822eeb279
LordMungi <lrdmungi@gmail.com>
2026-04-18 15:37:55 -0300

    Integrate handbrake in car.cs

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Handbrake handbrake;
+
     void Start()
     {
         
@@ -11,6 +12,8 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (handbrake.isPressed)
+        {
+        }
     }
 }
