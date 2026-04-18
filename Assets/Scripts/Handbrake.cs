a15eadc03dd5c1a6c8bcf2c0a4786fb95a1b8efa
LordMungi <lrdmungi@gmail.com>
2026-04-18 15:32:01 -0300

    Create isPressed

--- Assets/Scripts/Handbrake.cs
+++ Assets/Scripts/Handbrake.cs
@@ -7,12 +7,20 @@ public class Handbrake : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
     }
 }
