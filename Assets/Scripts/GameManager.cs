03bc58bbbfc55626e30307da8eb17deb771ab45a
LordMungi <lrdmungi@gmail.com>
2026-04-19 01:44:51 -0300

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
 
         if (obstacleQueued && timer >= timeOfNextObstacle)
         {
-            obstacleInstance = SpawnRoadEntity(sign, obstaclePathLeft, QueueSign);
+            obstacleInstance = SpawnRoadEntity(obstacle, obstaclePathLeft, QueueSign);
             obstacleQueued = false;
         }
 
