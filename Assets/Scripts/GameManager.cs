996857147c1cd6f4ae3b1615318fd5760209e8e1
LordMungi <lrdmungi@gmail.com>
2026-04-18 23:52:51 -0300

    Make so signs follow their path

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] GameObject Sign;
+    [SerializeField] GameObject PathLeft;
+
+    [SerializeField] float SIGN_SPEED;
 
     private float timeOfNextSign;
     private bool signQueued;
@@ -11,12 +15,15 @@ public class GameManager : MonoBehaviour
 
     private GameObject signInstance;
 
+    Queue<Transform> pathLeft;
+
     private float timer;
 
     void Start()
     {
         timer = 0.0f;
         QueueSign();
+
     }
     void Update()
     {
@@ -26,18 +33,48 @@ public class GameManager : MonoBehaviour
         {
             SpawnSign();
         }
+
+        FollowPath(pathLeft, signInstance);
     }
 
     void QueueSign()
     {
-        timeOfNextSign = timer + 5;
         signQueued = true;
+        timeOfNextSign = timer + 5;
     }
 
     void SpawnSign()
     {
-        signInstance = Instantiate(Sign);
         signQueued = false;
-        QueueSign();
+        signInstance = Instantiate(Sign);
+
+        pathLeft = CreatePathQueue(PathLeft);
+    }
+
+    private void FollowPath(Queue<Transform> path, GameObject sign)
+    {
+        if (sign != null)
+        {
+            sign.transform.position = Vector3.MoveTowards(sign.transform.position, path.Peek().position, Time.deltaTime * SIGN_SPEED);
+            if (sign.transform.position == path.Peek().position)
+            {
+                path.Dequeue();
+            }
+            if (path.Count == 0)
+            {
+                Destroy(sign);
+                QueueSign();
+            }
+        }
+    }
+
+    private Queue<Transform> CreatePathQueue(GameObject path)
+    {
+        Queue<Transform> pathQueue = new Queue<Transform>();
+        foreach (Transform child in path.transform)
+        {
+            pathQueue.Enqueue(child);
+        }
+        return pathQueue;
     }
 }
