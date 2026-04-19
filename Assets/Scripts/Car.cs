08dfda198cee8b2327e599b0ea726f32de0f3965
Suarez Nahuel <namof0315@gmail.com>
2026-04-19 14:23:28 -0300

    Add lose condition

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -78,10 +78,19 @@ public class Car : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Trigger");
+        if (lives <= MAX_LIVES && lives > 0)
+        {
+            lives--;
+        }
+        else
+        {
+            GameOver();
+        }
+        Debug.Log(lives);
     }
 
     private void GameOver()
     {
-
+        //Debug.Log("Perdiste perro!");
     }
 }
