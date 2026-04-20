e87d376a6173628eb18d19108554bb5faec02bb4
Mer1203 <mechiramirezdiaz@gmail.com>
2026-04-20 01:39:31 -0300

    Add New Background and Car

--- Assets/Scripts/Handbrake.cs
+++ Assets/Scripts/Handbrake.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class Handbrake : MonoBehaviour
 {
     [field: SerializeField] public bool isPressed { get; private set; } = false;
+    [SerializeField] private SpriteRenderer brake;
+    [SerializeField] private Sprite brakeOn;
+    [SerializeField] private Sprite brakeOff;
 
-    void Start()
+   void Start()
     {
     }
 
@@ -15,10 +18,12 @@ public class Handbrake : MonoBehaviour
     private void OnMouseDown()
     {
         isPressed = true;
+        brake.sprite = brakeOn;
     }
 
     private void OnMouseUp()
     {
         isPressed = false;
+        brake.sprite = brakeOff;
     }
 }
